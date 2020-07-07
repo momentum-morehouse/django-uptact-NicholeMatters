346ad98c5e6c009ca054c4ccda403cb4cbe29e24
Nichole Ross <thisnicholematters@gmail.com>
2020-07-07 05:17:24 +0000

    assignment 1 completed

--- contacts/forms.py
+++ contacts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Contact
+import datetime
 
 
 class ContactForm(forms.ModelForm):
@@ -14,4 +15,18 @@ class ContactForm(forms.ModelForm):
             'zip_code',
             'phone_number',
             'email',
+            'birthday',
+            'note'
         ]
+
+        widgets = {
+        'birthday': forms.DateInput(format=('%m/%d/%Y'), attrs={'class':'form-control', 'placeholder':'Select a date', 'type':'date'}),
+        }
+         
+class DateForm(forms.Form):
+   day = forms.DateField(initial=datetime.date.today)
+print(DateForm())       
+        
+
+#class Note(forms.Form):
+   #Contact = forms.ForeignKey(ContactForm, on_delete= forms.CASCADE)
