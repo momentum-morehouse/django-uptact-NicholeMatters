52f46e26eed499ad456d00d5280f2fe9754ab8f5
Nichole Ross <thisnicholematters@gmail.com>
2020-07-09 00:12:48 +0000

    updated url... site not working and there are no errors rn

--- uptact/settings.py
+++ uptact/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-uptact-nicholematters--nicholematters.repl.co']
+ALLOWED_HOSTS = ['django-uptact-nicholematters--nicholematters.repl.co', 'django-uptact-nicholematters.nicholematters.repl.co']
 
 # Application definition
 
